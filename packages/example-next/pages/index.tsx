8c80a8c7080c955524e3434b17aa78cdd62f24ba
Himanshu Devrani <himanshudevrani@192.168.1.3>
2022-10-20 11:27:21 +0530

    fix

--- packages/example-next/pages/index.tsx
+++ packages/example-next/pages/index.tsx
@@ -1,9 +1,4 @@
 import BitskiWalletCard from '../components/connectorCards/BitskiWalletCard'
-import CoinbaseWalletCard from '../components/connectorCards/CoinbaseWalletCard'
-import GnosisSafeCard from '../components/connectorCards/GnosisSafeCard'
-import MetaMaskCard from '../components/connectorCards/MetaMaskCard'
-import NetworkCard from '../components/connectorCards/NetworkCard'
-import WalletConnectCard from '../components/connectorCards/WalletConnectCard'
 import ProviderExample from '../components/ProviderExample'
 
 export default function Home() {
@@ -11,11 +6,6 @@ export default function Home() {
     <>
       <ProviderExample />
       <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
-        <MetaMaskCard />
-        <WalletConnectCard />
-        <CoinbaseWalletCard />
-        <NetworkCard />
-        <GnosisSafeCard />
         <BitskiWalletCard/>
       </div>
     </>
